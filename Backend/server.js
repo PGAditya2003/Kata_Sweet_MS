91e42e8d8e2667193816612eb2e56f69ecc5dc44
NME <adimaharshi20@gmail.com>
2025-09-25 11:05:46 +0530

    feat: mocha , chai , supertest , sinon dependencies added for unit testing

--- Backend/server.js
+++ Backend/server.js
@@ -5,9 +5,13 @@ const connectDB = require("./config/db");
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
-// connect to database
-connectDB(); 
+// Connect to DB
+connectDB();
+
+// Auth routes
+app.use('/api/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
